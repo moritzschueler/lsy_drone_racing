9b07df32a18044b503aaaeb1ad1278bba345fd2f
philippreinig <philipp.reinig@gmail.com>
2026-07-06 17:54:35 +0200

    Added logging of average num of gates passed to sim.py

--- scripts/sim.py
+++ scripts/sim.py
@@ -72,6 +72,7 @@ def simulate(
     env = JaxToNumpy(env)
 
     ep_times = []
+    gates_passed = []
     for _ in range(n_runs):  # Run n_runs episodes with the controller
         obs, info = env.reset()
         controller: Controller = controller_cls(obs, info, config)
@@ -102,6 +103,8 @@ def simulate(
         controller.episode_reset()
         ep_times.append(curr_time if obs["target_gate"] == -1 else None)
 
+        gates_passed.append(obs["target_gate"] if obs["target_gate"] != -1 else len(config.env.track.gates))
+    print(f"Average number of gates passed: {round(sum(gates_passed)/n_runs, 1)}")
     # Close the environment
     env.close()
     return ep_times
